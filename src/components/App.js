3e36b5c46fb342bfab2eac4b647bd35b55efae9b
Yazmin Perez <zoeperez333@gmail.com>
2022-08-01 15:19:53 -0700

    create components directory & move App.js.

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App(){
+  const name = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      {/* This is a JSX comment. */}
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
