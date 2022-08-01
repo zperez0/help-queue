649564dda68865a94378ec164f8a1675988201a2
Yazmin Perez <zoeperez333@gmail.com>
2022-08-01 16:38:23 -0700

    [WIP] add mainTicketList & update TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,9 +1,35 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const mainTicketList = [
+  {
+  names: "Thato and Haley",
+  location: "3A",
+  issue: "Firebase won't save record. Help."
+  },
+  {
+    names: "Sleater & Kinney",
+    location: "4B",
+    issue: "Prop types are throwing an error."
+  },
+  {
+    names: "Imani & Jacob",
+    location: "9F",
+    issue: "Child component isn't rendering."
+  }
+];
+
 function TicketList() {
   return (
-    <Ticket />
+  <React.Fragment>
+    <hr/>
+        {mainTicketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue}
+            key={index}/>
+        )}
+  </React.Fragment>
   );
 }
 
