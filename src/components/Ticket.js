7826ccfb735fdd1cec659246a0ee470a438af5dc
Yazmin Perez <zoeperez333@gmail.com>
2022-08-01 16:39:03 -0700

    [WIP] update Ticket & add Ticket.propTypes

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,16 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ticket() {
-  const name = "Thato";
-  const name2 = "Haley";
+function Ticket(props){
   return (
     <React.Fragment>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr />
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
     </React.Fragment>
   );
 }
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
 
 export default Ticket;
