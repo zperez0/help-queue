76471143ba337e5820322280406bff59758bfbaa
Yazmin Perez <zoeperez333@gmail.com>
2022-08-02 15:31:24 -0700

    add TicketControl

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
 
 function App(){
-  const name = "Thato";
-  const name2 = "Haley";
-  return (
+  return ( 
     <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
-      <hr/>
+      <Header />
+      <TicketControl />
     </React.Fragment>
   );
 }
