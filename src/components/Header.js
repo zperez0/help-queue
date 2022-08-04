4d66b5fb3dffb733049a9dbf2a072e3819307938
Yazmin Perez <zoeperez333@gmail.com>
2022-08-03 17:56:42 -0700

    add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,18 @@ import React from "react";
 import ticketsImage from "./../img/tickets.png";
 
 function Header(){
+  const headerStyles = {
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
   return (
+    <div style={headerStyles}>
     <React.Fragment>
       <h1>Help Queue</h1>
       <img src={ticketsImage} alt="An image of tickets" />
     </React.Fragment>
+    </div>
   );
 }
 
