cfc30be3695842773f2c20c476d1d97a0210819b
Yazmin Perez <zoeperez333@gmail.com>
2022-08-07 17:06:13 -0700

    update NewTicketForm

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { v4 } from "uuid";
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 // child component
 
@@ -8,10 +9,10 @@ function NewTicketForm(props) {
   function handleNewTicketFormSubmission(event) {
     event.preventDefault(); // default behavior of an HTML submit button -> to submit data & refresh page -> to prevent refresh, add preventDefault()
     props.onNewTicketCreation({
-      names: event.target.names.value, 
-      location: event.target.location.value, 
-      issue: event.target.issue.value, 
-      id: v4()
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4(),
     });
     console.log(event.target.names.value); // event.target gives us access to the submit event that was just fired.
     console.log(event.target.location.value);
@@ -20,18 +21,16 @@ function NewTicketForm(props) {
 
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}>
-        <input type="text" name="names" placeholder="Pair Names" />
-        <input type="text" name="location" placeholder="Location" />
-        <textarea name="issue" placeholder="Describe your issue." />
-        <button type="submit">Submit</button>
-      </form>
+      <ReusableForm
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!"
+      />
     </React.Fragment>
   );
 }
 
 NewTicketForm.propTypes = {
-  onNewTicketCreation: PropTypes.func
+  onNewTicketCreation: PropTypes.func,
 };
 
 export default NewTicketForm;
